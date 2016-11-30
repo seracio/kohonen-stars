6f1af6680ae97363d291b9d57862e869bc916054
nicolasmondon <nicolasmondon@gmail.com>
2016-11-30 20:55:31 +0100

    readme

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ class App extends Component {
     return (
       <div>
         <svg width={'100%'} height={'100%'} viewBox={`0 0 ${width} ${height}`} style={{
-          backgroundColor: 'rgb(18, 26, 70)',
+          backgroundColor: 'rgb(25, 38, 109)',
         }}>
           <defs>
             <filter id="glow">
